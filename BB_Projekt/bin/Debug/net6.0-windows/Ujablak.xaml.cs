5259de739cd45ffe13a7abf122a1ca832ccfdce3
budaizalan <budai.zalan@students.jedlik.eu>
2023-11-14 20:08:06 +0100

    Összes CSS, Készítők oldal, Kódrendezés kész

---
+++ BB_Projekt/bin/Debug/net6.0-windows/Ujablak.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BB_Projekt
+{
+    /// <summary>
+    /// Interaction logic for Ujablak.xaml
+    /// </summary>
+    public partial class Ujablak : Window
+    {
+        public Megoldas megoldas;
+
+        private List<int> napok;
+
+        public ObservableCollection<string> ujtermekseged = new ObservableCollection<string>();
+        public Ujablak()
+        {
+            InitializeComponent();
+            napok_Feltolt();
+            megoldas = new Megoldas();
+            this.DataContext = megoldas;
+        }
+
+        private void ujMegseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void ujFelvetelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(ujTermekTxtbx.Text) && int.TryParse(ujArTxtbx.Text, out int ar) && int.TryParse(ujEvTxtbx.Text, out int ev) && NapEllenorzo(ujHonapCbbx.SelectedItem.ToString(), int.Parse(ujNapCbbx.SelectedItem.ToString())))
+            {
+                MessageBoxResult result = MessageBox.Show("Biztos fel akarja venni a listába az új terméket?", "Mentés", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    this.DialogResult = true;
+                    this.Close();
+                }
+            }
+            //if (!string.isnullorempty(ujtermektxtbx.text))
+            //{
+            //    if (int.tryparse(ujartxtbx.text, out int ar))
+            //    {
+            //        if (napellenorzo(ujhonapcbbx.selecteditem.tostring(), int.parse(ujnapcbbx.selecteditem.tostring())))
+            //        {
+            //            messageboxresult result = messagebox.show("biztos fel akarja venni a listába az új terméket?", "mentés", messageboxbutton.yesno, messageboximage.question);
+            //            if (result == messageboxresult.yes)
+            //            {
+            //                this.dialogresult = true;
+            //                this.close();
+            //            }
+            //        }
+            //    }
+            //}
+            else
+            {
+                MessageBox.Show("Hibás adatatot adott meg az egyik bekérésnél", "Hiba!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void napok_Feltolt()
+        {
+            napok = new List<int>();
+            for (int i = 1; i <= 31; i++)
+            {
+                napok.Add(i);
+            }
+            ujNapCbbx.ItemsSource = napok;
+        }
+
+        private bool NapEllenorzo(string honap, int nap)
+        {
+            if ((honap == "Április" || honap == "Június" || honap == "Szeptember" || honap == "November") && nap > 30)
+            {
+                return false;
+            }
+            if (honap == "Február" && nap > 28)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public Termek UjTermek()
+        {
+            ujtermekseged = megoldas.ujtermekek;
+            ujtermekseged[1] = (int.Parse(ujtermekseged[1]) + 1).ToString();
+            ujtermekseged[2] = (int.Parse(ujtermekseged[2]) + 1).ToString();
+            string segedSor = $"{ujtermekseged[0]};{((int.Parse(ujtermekseged[1]) <= 9) ? "0" : "")}{ujtermekseged[1]}-{((int.Parse(ujtermekseged[2]) <= 9) ? "0" : "")}{ujtermekseged[2]};{ujtermekseged[3]};{ujtermekseged[4]}";
+            return new Termek(segedSor);
+        }
+    }
+}
